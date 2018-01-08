5c102ed25be89a03abb3f655bc2edb3f59edd7e0
Dawson <dawsonrichey89@gmail.com>
2018-01-08 01:56:54 +0000

    coupon code text changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+// When the button is pressed 
+$('.spoiler').on('click', 'button', function(){
+  //Show the spoiler text
+  $(this).prev().show();
+  //Hide the "Reveal Spoiler" button
+  $(this).hide();
+});
 
+// Create the "Reveal Spoiler" button
+const $button = $('<button>Reveal Discount</button>');
+//Append to web page
+$('.spoiler').append($button);
+
+//Hide the spoiler text
+$('.spoiler span').hide();
 
 
 
