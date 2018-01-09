cc3a6abd4e8f34c114dae4c37e3b6a832185ccb9
Dawson <dawsonrichey89@gmail.com>
2018-01-09 02:52:52 +0000

    added quiz

---
+++ quiz/js/quiz.js
@@ -0,0 +1,60 @@
+var questions = [
+  ['How many states are in the United States?', 50],
+  ['How many continents are there?', 7],
+  ['How many legs does an insect have?', 6]
+];
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var correct = [];
+var wrong = [];
+var wronganswer = [];
+
+
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+ var listHTML = '<ol>';
+  for (var i = 0; i < arr.length; i += 1) {
+    listHTML += '<li>' + arr[i] + '</li>';
+  }
+  listHTML += '</ol>';
+  return listHTML;
+}
+
+for (var i = 0; i < questions.length; i += 1) {
+  question = questions[i][0];
+  answer = questions[i][1];
+  response = prompt(question);
+  response = parseInt(response);
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
+  } else {
+    wrong.push(question);
+    wronganswer.push(answer);
+  }
+}
+
+html = "You got " + correctAnswers + " question(s) right."
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += buildList(answer);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
+html += '<h2>The correct answers:</h2>';
+
+html += buildList(wronganswer);
+print(html);
+
+
+
+
+
+
+
+
