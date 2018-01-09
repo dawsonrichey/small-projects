25febc71df0007d706a955c1eb6cc8221883ae80
Dawson <dawsonrichey89@gmail.com>
2018-01-09 02:26:32 +0000

    formatting changes

---
+++ javascript/style.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+let greeting = 'Welcome to my website, '
+const namePersonal = prompt('Enter Your Name','');
+let message = greeting + namePersonal + '.';
+/*let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+alert(user);
+alert(age);
+alert(message);*/
+alert(greeting + namePersonal);
+alert(message);
---
+++ javascript/style2.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let admin = 'nothing';
+let name = 'John';
+admin = name;
+alert(admin);
+
+const ourPlanet = 'Earth';
+let currentVisitor = "";
+
+/*let greeting = 'Welcome to my website, ';
+const namePersonal = prompt('Enter Your Name','');
+let message = greeting + namePersonal + '.';*/
+/*let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+alert(user);
+alert(age);
+alert(message);*/
+/*
+alert(greeting + namePersonal);
+alert(message);
+*/
